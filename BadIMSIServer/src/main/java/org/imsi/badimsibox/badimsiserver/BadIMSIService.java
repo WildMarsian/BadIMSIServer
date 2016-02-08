605b89e4ef325129148e0de95e33567937da464d
johnny <jwankutk@etud.u-pem.fr>
2016-02-08 18:07:05 +0100

    adding the static routes

--- BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
@@ -1,53 +1,58 @@
 package org.imsi.badimsibox.badimsiserver;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.Session;
-import io.vertx.ext.web.handler.CookieHandler;
-import io.vertx.ext.web.handler.SessionHandler;
-import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.handler.StaticHandler;
+
 
 public class BadIMSIService extends AbstractVerticle {
 	
-	@Override
-	public void start() {
-		Router router = Router.router(vertx);
-		router.route().handler(CookieHandler.create());
-		router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
-		
-		/*
-		 * Ici on definit les differentes URI et comment elles seront gerrés par
-		 * vertx a l'aide du Handler. Ici il s'agit d'une méthode get car il n'y
-		 * a pas de modifications des donnés au niveau du service. Les parties
-		 * de l'URI contenant des ":" sont des paramètres.
-		 */
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+        
+        // Just us me for tests. Remove me at the end. Thank you :)
+        router.get("/hello/world/:name/").handler(rc -> {
+            String name = rc.request().getParam("name");
+            rc.response()
+                    .putHeader("content-type", "application/json")
+                    .end(new JsonObject().put("key", "Bonjour " + name).encode());
+        });
+        
+        router.get("/user/:username/password/:salted").handler(rc -> {
+        	JsonObject authInfo = new JsonObject().put("username", "zak").put("password", "mypassword");
+			
+        	AuthProvider authProvider = new AuthProvider() {
+    			
+    			@Override
+    			public void authenticate(JsonObject arg0, Handler<AsyncResult<User>> arg1) {
+    				// TODO Auto-generated method stub
+    				
+    			}
+    	};
 
-		router.get("/hello/world/:name/").handler(rc -> {
-			// Recuperation du parametre name
-			String name = rc.request().getParam("name");
+        	
+        authProvider.authenticate(authInfo, res -> {
+        		  if (res.succeeded()) {
 
-			rc.response().putHeader("content-type", "application/json")
-					.end(new JsonObject().put("key", "Bonjour " + name).encode());
-		});
+        		    User user = res.result();
 
-		router.get("/user/:username/password/:salted/").handler(rc -> {
-			String user = rc.request().getParam("username");
-			String password = rc.request().getParam("salted");
-			router.route().handler(routingContext -> {
-				
-				System.out.println("test");
-				
-			});
-			rc.response().putHeader("content-type", "application/json")
-					.end(new JsonObject().put("user", user)
-							.put("password", password).encode());
-		});
+        		    System.out.println("User " + user.principal() + " is now authenticated");
 
-		/*
-		 * Ici on définit la methode appelee lors de l'ecoute et sur quel port
-		 * cette écoute s'effectue
-		 */
-		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-	}
+        		  } else {
+        		    res.cause().printStackTrace();
+        		  }
+        		});
+        });
+
+        // Creating the static route towards the webroot folder 
+        router.route().handler(StaticHandler.create());
+        
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+    }
 }
