ed556b7f597287251e787142770e3c930b2cbacc
Zak <zdjeblou@etud.u-pem.fr>
2016-02-08 17:57:06 +0100

    Auth

--- BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/AuthentificationHandler.java
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/AuthentificationHandler.java
@@ -1,5 +1,12 @@
 package org.imsi.badimsibox.badimsiserver;
 
 public class AuthentificationHandler {
-
+	public static boolean createUser(String username, String saltedPassword) {
+		return false;
+	}
+	public static boolean authenticateUser(String username, String saltedPassword) {
+		
+		
+		return false;
+	}
 }
