8232c8a24b211df69696120fd33722813b8a876f
Zak <zdjeblou@etud.u-pem.fr>
2016-02-09 11:01:39 +0100

    Auth first try

--- BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
@@ -1,60 +1,50 @@
 package org.imsi.badimsibox.badimsiserver;
 
+import org.apache.shiro.realm.Realm;
+
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
-import io.vertx.ext.auth.User;
+import io.vertx.ext.auth.shiro.ShiroAuth;
+import io.vertx.ext.auth.shiro.ShiroAuthOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
-
+import io.vertx.ext.web.handler.UserSessionHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 
 public class BadIMSIService extends AbstractVerticle {
-	
-    @Override
-    public void start() {
-        Router router = Router.router(vertx);
-        
-        // Just us me for tests. Remove me at the end. Thank you :)
-        router.get("/hello/world/:name/").handler(rc -> {
-            String name = rc.request().getParam("name");
-            rc.response()
-                    .putHeader("content-type", "application/json")
-                    .end(new JsonObject().put("key", "Bonjour " + name).encode());
-        });
-        
-        router.get("/user/:username/password/:salted").handler(rc -> {
-        	JsonObject authInfo = new JsonObject().put("username", "zak").put("password", "mypassword");
-			
-        	AuthProvider authProvider = new AuthProvider() {
-    			
-    			@Override
-    			public void authenticate(JsonObject arg0, Handler<AsyncResult<User>> arg1) {
-    				// TODO Auto-generated method stub
-    				
-    			}
-    	};
-    	
-    	
 
-        	
-        authProvider.authenticate(authInfo, res -> {
-        		  if (res.succeeded()) {
+	@Override
+	public void start() {
+		Router router = Router.router(vertx);
+		
+		// Let's set up the cookies, request bodies and sessions
+		router.route().handler(CookieHandler.create());
+		router.route().handler(BodyHandler.create());
+		router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
+		
+		// Simple auth using properties file for user/role info
+		Realm realm = new R
+		
+		// Using session handler to make sure user is stored in the session between requests
+		// router.route().handler(UserSessionHandler.create(authProvider));
+		
+		
 
-        		    User user = res.result();
+		// Just us me for tests. Remove me at the end. Thank you :)
+		router.get("/hello/world/:name/").handler(rc -> {
+			String name = rc.request().getParam("name");
+			rc.response().putHeader("content-type", "application/json")
+					.end(new JsonObject().put("key", "Bonjour " + name).encode());
+		});
 
-        		    System.out.println("User " + user.principal() + " is now authenticated");
+		// Creating the static route towards the webroot folder
+		router.route().handler(StaticHandler.create());
 
-        		  } else {
-        		    res.cause().printStackTrace();
-        		  }
-        		});
-        });
-
-        // Creating the static route towards the webroot folder 
-        router.route().handler(StaticHandler.create());
-        
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-    }
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+	}
 }
