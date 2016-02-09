6b115113be0fe74b1feb669b3811a038bd83005a
Zak <zdjeblou@etud.u-pem.fr>
2016-02-09 11:54:00 +0100

    Beginning of auth

--- BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
@@ -1,16 +1,16 @@
 package org.imsi.badimsibox.badimsiserver;
 
-import org.apache.shiro.realm.Realm;
-
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.shiro.PropertiesProviderConstants;
 import io.vertx.ext.auth.shiro.ShiroAuth;
-import io.vertx.ext.auth.shiro.ShiroAuthOptions;
 import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.FormLoginHandler;
+import io.vertx.ext.web.handler.RedirectAuthHandler;
 import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.UserSessionHandler;
@@ -28,23 +28,25 @@ public class BadIMSIService extends AbstractVerticle {
 		router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
 		
 		// Simple auth using properties file for user/role info
-		Realm realm = new R
+		AuthProvider authProvider = ShiroAuth.create(vertx, ShiroAuthRealmType.PROPERTIES, new JsonObject().put(PropertiesProviderConstants.PROPERTIES_PROPS_PATH_FIELD, "classpath:auth.properties"));
 		
 		// Using session handler to make sure user is stored in the session between requests
-		// router.route().handler(UserSessionHandler.create(authProvider));
+		router.route().handler(UserSessionHandler.create(authProvider));
 		
-		
-
-		// Just us me for tests. Remove me at the end. Thank you :)
-		router.get("/hello/world/:name/").handler(rc -> {
-			String name = rc.request().getParam("name");
-			rc.response().putHeader("content-type", "application/json")
-					.end(new JsonObject().put("key", "Bonjour " + name).encode());
-		});
+		router.route("/master/*").handler(RedirectAuthHandler.create(authProvider, "/index.html"));
 
 		// Creating the static route towards the webroot folder
+		router.route("/master/*").handler(StaticHandler.create().setCachingEnabled(false).setWebRoot("webroot"));
+		
+		router.route("/loginhandler").handler(FormLoginHandler.create(authProvider));
+		
+		router.route("/logout").handler(context -> {
+			context.clearUser();
+			
+			context.response().putHeader("location", "/").setStatusCode(302).end();
+		});
+		
 		router.route().handler(StaticHandler.create());
-
 		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 	}
 }
