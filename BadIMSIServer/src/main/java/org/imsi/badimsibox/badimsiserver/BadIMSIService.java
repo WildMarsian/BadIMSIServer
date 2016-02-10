ccf13e57ad1a034d02e67c304e5bdedfb1a90542
johnny <jwankutk@etud.u-pem.fr>
2016-02-10 15:37:59 +0100

    adding zak to the team and python caller class. Integration of python caller in comment.

--- BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/BadIMSIService.java
@@ -12,8 +12,15 @@ public class BadIMSIService extends AbstractVerticle {
 		Router router = Router.router(vertx);
 		
 		router.get("/master/session/:state").handler(rc -> {
-    		String name = rc.request().getParam("state");
-
+			String name = rc.request().getParam("state");
+    		if(name.equals("start")) {
+    			/* Code to start session */
+    		}
+    		
+    		if(name.equals("stop")) {
+    			/* Code to stop session */
+    		}
+    		
     		// We have to give the right response
     		rc.response()
             	.putHeader("content-type", "application/json")
@@ -22,33 +29,88 @@ public class BadIMSIService extends AbstractVerticle {
             	.encode());
     	});  	
     	
-		router.get("/master/map/:locate").handler(rc -> {
-    		String name = "48.839915,2.5842899,17z";
-    		// We have to give the right response
+		router.get("/master/map/:locate").handler(rc -> {	
+    		// Actually, this is an example: Remove me when you put the right code
+			String name = "48.839915,2.5842899,17z";
     		rc.response()
             	.putHeader("content-type", "application/json")
             	.end(new JsonObject().put("location", name)
             	.encode());
+		
+			/*	
+			// Example to call python files from Java
+			String[] pythonLocationScript = {"/path/to/script/python/locate_me.py","-i","input","-o", "output"};
+    		PythonCaller pc = new PythonCaller(pythonLocationScript);
+    		int exitValue = -1;
+    		try {
+				exitValue = pc.process();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    		
+    		if(exitValue == 0) {
+        		rc.response()
+            	.putHeader("content-type", "application/json")
+            	.end(new JsonObject().put("location", pc.getResultSb())
+            	.encode());
+    		}	
+    		*/
     	});
     	
     	router.get("/master/sniffing/:state").handler(rc -> {
+    		
     		String name = rc.request().getParam("state");
-
-    		// We have to give the right response
     		rc.response()
+        	.putHeader("content-type", "application/json")
+        	.end(new JsonObject().put("state", name)
+        	.encode());
+    		
+    		/*
+    		String[] pythonLocationScript = {PythonCaller.getContextPath()+"badimsicore","-l",name};
+    		PythonCaller pc = new PythonCaller(pythonLocationScript);
+    		int exitValue = -1;
+    		try {
+				exitValue = pc.process();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    		
+    		if(exitValue == 0) {
+        		rc.response()
             	.putHeader("content-type", "application/json")
-            	.end(new JsonObject().put("state", name)
+            	.end(new JsonObject().put("sniffing", pc.getResultSb())
             	.encode());
+    		}	
+    		*/
+
     	});
     	
     	router.get("/master/jamming/:state").handler(rc -> {
     		String name = rc.request().getParam("state");
-
+    		
     		// We have to give the right response
     		rc.response()
             	.putHeader("content-type", "application/json")
             	.end(new JsonObject().put("state", name)
             	.encode());
+    		
+    		/*
+    		String[] pythonLocationScript = {PythonCaller.getContextPath()+"badimsicore","-j",name};
+    		PythonCaller pc = new PythonCaller(pythonLocationScript);
+    		int exitValue = -1;
+    		try {
+				exitValue = pc.process();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    		
+    		if(exitValue == 0) {
+        		rc.response()
+            	.putHeader("content-type", "application/json")
+            	.end(new JsonObject().put("jamming", pc.getResultSb())
+            	.encode());
+    		}	
+    		*/
     	});
     	
     	router.get("/master/fakebts/:state").handler(rc -> {
@@ -59,6 +121,24 @@ public class BadIMSIService extends AbstractVerticle {
             	.putHeader("content-type", "application/json")
             	.end(new JsonObject().put("state", name)
             	.encode());
+    		
+    		/*
+    		String[] pythonLocationScript = {PythonCaller.getContextPath()+"badimsicore","-b",name};
+    		PythonCaller pc = new PythonCaller(pythonLocationScript);
+    		int exitValue = -1;
+    		try {
+				exitValue = pc.process();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    		
+    		if(exitValue == 0) {
+        		rc.response()
+            	.putHeader("content-type", "application/json")
+            	.end(new JsonObject().put("fake bts", pc.getResultSb())
+            	.encode());
+    		}	
+    		*/
     	});
 
 		
---
+++ BadIMSIServer/src/main/java/org/imsi/badimsibox/badimsiserver/PythonCaller.java
@@ -0,0 +1,109 @@
+package org.imsi.badimsibox.badimsiserver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Objects;
+
+
+/**
+ * Python Caller is a class that calls python code and return the result in the stdout.
+ */
+
+public class PythonCaller {
+
+	
+	private static String contextPath = "/opt/badimsicore/";
+	
+    private final String program = "python";
+    private final String[] scriptPath;  
+    private final String cmd;
+    private final StringBuilder resultSb = new StringBuilder();
+
+    public PythonCaller(String[] scriptPath) {
+        if(scriptPath.length == 0) {
+        	throw new IllegalArgumentException("No args found");
+        }
+    	
+    	this.scriptPath = Objects.requireNonNull(scriptPath);  
+    	StringBuilder sb = new StringBuilder();
+        sb.append(program);
+        for (String string : scriptPath) {
+        	sb.append(" ");
+        	sb.append(string);
+		}
+       
+        this.cmd = sb.toString();
+    }
+
+    public int process() throws IOException {
+        Runtime rt = Runtime.getRuntime();
+
+        Process pr = rt.exec(cmd);
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+        String line;
+        while ((line = bfr.readLine()) != null) {
+            System.out.println(line);
+            resultSb.append(line+"\n");
+        }
+
+        int val = 1;
+        try {
+            val = pr.waitFor(); 
+            return val;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+            return -1;
+    }
+
+    public static String getContextPath() {
+		return contextPath;
+	}
+    
+    public String getResultSb() {
+		return resultSb.toString();
+	}
+    
+    
+    public static String getCleanPath() {
+        URL location = PythonCaller.class.getProtectionDomain().getCodeSource()
+                .getLocation();
+        String path = location.getFile();
+        return new File(path).getParent()+"/"+new File(path).getName();
+    }
+
+    public static void main(String[] args) {
+        if(args.length == 0) {
+            noArgsError();
+        }
+
+        if(args.length > 0) {
+            PythonCaller pc = new PythonCaller(args);
+            try {
+                int exitValue = pc.process();
+                System.out.println("Result: "+pc.getResultSb());
+                System.out.println("Exit value : "+exitValue);
+
+            } catch (IOException e) {
+                errorOnScript(args);
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void noArgsError() {
+        System.out.println("No arguments were found");
+    }
+
+    private static void errorOnScript(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (String arg : args) {
+            sb.append(arg+" ");
+        }
+        System.out.println("Error on the script : no script found for "+sb.toString());
+    }
+}
